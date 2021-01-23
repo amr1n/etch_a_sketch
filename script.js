430281d43b3693bb979d54eaad17701d31c63a04
amr1n <52592047+amr1n@users.noreply.github.com>
2021-01-23 15:14:54 +0600

    created the reset button

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 let container = document.querySelector('.container');
-//create the divs and add event to them
-for (i = 0; i < 256; i++) {
-	let div = document.createElement('div'); 
-	div.classList.add('gridItem');
-	
-	div.addEventListener('mouseover', function () {
-		div.style = 'background: white';
-	})
+let resetBtn = document.querySelector('.resetBtn');
+let newGrid = document.querySelector('.newGrid');
 
-	container.appendChild(div);
+//create the divs and add event to them
+let allGridItems = [];
+for (i = 0; i < 256; i++) {
+	let gridItem = document.createElement('div'); 
+		gridItem.classList.add('gridItem');
+
+	allGridItems.push(gridItem);
+	container.appendChild(gridItem);
+
+} 
+
+// gridItem is an element fo allGridItems thats why the event is inside the forEach loop
+allGridItems.forEach( function(gridItem) {
+	gridItem.addEventListener('mouseover', () => {
+		gridItem.classList.remove('resetSketchBoard');
+		gridItem.classList.add('mouseover');
+	});
+});
+
+
+function resetSketchBoard (){
+	allGridItems.forEach( gridItem => {
+		gridItem.classList.remove('mouseover');
+		gridItem.classList.add('resetSketchBoard');
+	});
 }
+
+resetBtn.addEventListener('click', resetSketchBoard)
 
