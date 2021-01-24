2f38fc567d675fec1239ab36ada40127d17b72d9
amr1n <52592047+amr1n@users.noreply.github.com>
2021-01-24 11:49:28 +0600

    added the functionality for newGridSize and completed the project

--- script.js
+++ script.js
@@ -1,33 +1,59 @@
 let container = document.querySelector('.container');
 let resetBtn = document.querySelector('.resetBtn');
-let newGrid = document.querySelector('.newGrid');
+let newGrid = document.querySelector('.newGrid'); 
 
-//create the divs and add event to them
-let allGridItems = [];
-for (i = 0; i < 256; i++) {
-	let gridItem = document.createElement('div'); 
+function defaultGrid() {
+	createGrid(16);
+	gridItemSize(16)
+}
+
+function gridItemSize(itemSize) {
+	container.style.gridTemplateColumns = `repeat(${itemSize}, 1fr)`;
+}
+
+let allGridItems;
+function createGrid(gridSize) {
+	 allGridItems = []; 
+	for (i = 0; i < gridSize * gridSize; i++) {
+		let gridItem = document.createElement('div'); 
 		gridItem.classList.add('gridItem');
+		allGridItems.push(gridItem); 
+		container.appendChild(gridItem);
 
-	allGridItems.push(gridItem);
-	container.appendChild(gridItem);
-
-} 
-
-// gridItem is an element fo allGridItems thats why the event is inside the forEach loop
-allGridItems.forEach( function(gridItem) {
-	gridItem.addEventListener('mouseover', () => {
-		gridItem.classList.remove('resetSketchBoard');
-		gridItem.classList.add('mouseover');
-	});
-});
-
+		gridItem.addEventListener('mouseover', () => {
+			gridItem.classList.remove('resetSketchBoard');
+			gridItem.classList.add('mouseover_color');
+		});
+	} 
+}
 
 function resetSketchBoard (){
 	allGridItems.forEach( gridItem => {
-		gridItem.classList.remove('mouseover');
+		gridItem.classList.remove('mouseover_color');
 		gridItem.classList.add('resetSketchBoard');
 	});
 }
 
-resetBtn.addEventListener('click', resetSketchBoard)
+function createNewGrid() {
+	let newGridSize = prompt("Enter a number from 1-100");
+
+	if (newGridSize !== null) {
+		newGridSize = parseInt(newGridSize);
+		removeOldGridItems();
+		createGrid(newGridSize);
+		gridItemSize(newGridSize);
+	}
+}
+
+function removeOldGridItems() {
+	let containerArray = Array.from(container.childNodes);
+	containerArray.forEach((gridItem) => {
+		container.removeChild(gridItem);
+	});
+}
+
+window.addEventListener('load', defaultGrid);
+resetBtn.addEventListener('click', resetSketchBoard);
+newGrid.addEventListener('click', createNewGrid);
+
 
